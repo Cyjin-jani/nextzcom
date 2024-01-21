8bcf0557e600c7234aaec8e9a32ff3219f16f6f3
Cyjin-jani <jinjanic91@gmail.com>
2024-01-21 23:59:45 +0900

    c: 페러렐 라우팅의 기본 값 설정

---
+++ src/app/(beforeLogin)/@modal/default.tsx
@@ -0,0 +1,6 @@
+// 페러렐 라우트에 대한 기본값.
+// 페러렐 라우트에 page.tsx가 필요하지 않은 경우 사용한다.
+// 왜냐. 여기에선 로그인 클릭 시 모달이 떠야 하고, 기본 주소 (메인) (localhost:3000/) 에서는 아무것도 모달이 뜰 필요가 없기 때문.
+export default function Default() {
+  return null;
+}
