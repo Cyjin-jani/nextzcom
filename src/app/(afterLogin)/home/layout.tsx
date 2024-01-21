7d60756d1aee194085103e384300d79ad8ae8689
Cyjin-jani <jinjanic91@gmail.com>
2024-01-21 23:57:40 +0900

    u: layout 수정

--- src/app/(afterLogin)/home/layout.tsx
+++ src/app/(afterLogin)/home/layout.tsx
@@ -1,6 +1,6 @@
-import {ReactNode} from "react";
+import { ReactNode } from 'react';
 
-export default async function HomeLayout({children}: { children: ReactNode }) {
+export default async function HomeLayout({ children }: { children: ReactNode }) {
   return (
     <div>
       홈 레이아웃
@@ -8,3 +8,10 @@ export default async function HomeLayout({children}: { children: ReactNode }) {
     </div>
   );
 }
+
+// 페이지를 넘나들 때, layout은 리렌더링이 안되고, 여기 안의 페이지들만 리렌더링이 된다. (why?)
+// 만약, 페이지 넘어갈 때 마다 레이아웃도 리렌더를 하고 싶다면, template.tsx를 사용한다. (layout.tsx 대신 선택해야 함. 공존x)
+// 페이지 넘나들 때 공통적으로 무언가 수행하거나, 기록 등을 할 때 template을 사용.
+// ex. ga를 심어서 무언가 전달할때?
+// 페이지 넘나들 때 마다 리렌더라기 보다는 매번 새롭게 template이 mount된다고 보면 됨.
+// 근데 보통의 경우 template 대신 layout을 씀.
--- src/app/(beforeLogin)/layout.tsx
+++ src/app/(beforeLogin)/layout.tsx
@@ -1,12 +1,26 @@
-import {ReactNode} from "react";
+import { ReactNode } from 'react';
+import styles from '@/app/page.module.css';
 
-type Props = { children: ReactNode, modal: ReactNode };
-export default function Layout({ children }: Props) {
+// 페러렐 라우트
+// 동시에 두 페이지를 보여줄 수 있는 기술
+// 사용시 주의점: 폴더 디렉토리의 위계 구조가 중요함
+// layout, page.tsx가 @modal과 같은 폴더 내 형제 관계로 위치해 있어야 사용이 가능.
+
+// modal을 이렇게 넘겨줄 수 있게 된다.
+// 같은 폴더 내에서만 사용. 레이아웃에서 사용하는 것.
+// modal을 더 쓰고 싶다면 추가가 가능하다 (그러나 추천하지 않음 - 컨트롤이나 위계가 헷갈리게 될 것)
+// 추가 방법: @modal2이런 식으로 만들고 아래 Props에 modal2: ReactNode이런식으로 추가 후 사용
+type Props = { children: ReactNode; modal: ReactNode };
+export default function Layout({ children, modal }: Props) {
   return (
-    <div>
+    <div className={styles.container}>
+      {/* 비포 로그인 레이아웃 */}
       {children}
+      {modal}
+      {/* 모달 추가하면 이렇게 쓴다 - 당연히 Props 타입에도 추가하고.. */}
+      {/* {modal2} */}
     </div>
-  )
+  );
 }
 
 // 주소가 localhost:3001일 때는 children->page.tsx, modal->@modal/default.tsx
